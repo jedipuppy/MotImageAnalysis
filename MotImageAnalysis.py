166f1befd93cdf998fdcdea109055f1e07e894a9
jedipuppy <puppy@kaduo.jp>
2017-12-22 04:52:54 +0900

    improve for run28
    improve for run28

--- MotImageAnalysis.py
+++ MotImageAnalysis.py
@@ -38,6 +38,7 @@ class ImageAnalysis():
         self.upper_img = np.zeros((128,168),np.float64)
         self.lower_img = np.zeros((128,168),np.float64)
         self.bg_img = np.zeros((128,168),np.int8)
+        self.diff_img = np.zeros((128,168),np.int8)       
         self.loadImgAll(self.filename)
 
     def loadData(self):
@@ -112,18 +113,21 @@ class ImageAnalysis():
     def loadImgAll(self,filename):
         print("BG file number",self.background_file_number)
         self.bg_img = self.loadBG(filename,self.background_file_number)
-        file_number = self.start_number
+        file_number = 0
         while(True):
             loadFlag = self.loadImg(filename,file_number)
-            if loadFlag is 1 or (file_number > stop_number and stop_number != 0 ):
+            if loadFlag is 1:
                 break;
             file_number += 1
             print(file_number)
 
-            self.accum_img(self.img_array[-1],self.roi_array[-1])
+            if file_number > start_number and file_number <stop_number and stop_number != 0:
+                self.accum_img(self.img_array[-1],self.roi_array[-1])
         if self.upper_img_num != 0:
+            print ("accum upper",file_number)
             self.upper_img =  self.upper_img/self.upper_img_num
         if self.lower_img_num != 0:
+            print ("accum lower",file_number)
             self.lower_img = self.lower_img/self.lower_img_num
 
             self.diff_img = self.upper_img - self.lower_img
@@ -161,18 +165,17 @@ image_ana = ImageAnalysis(filename,upper_threshold,lower_threshold,background_fi
 fig = plt.figure(figsize=(14,9))
 fig.subplots_adjust(top = 0.930,bottom =0.095,left=0.060,right =0.940)
 suptitle = plt.suptitle("summary-"+filename+".png, ROI: x="+str(x)+",y="+str(y)+",x2="+str(x2)+",y2="+str(y2) , x = 0.5, y = 0.97, fontsize=18)
-gs = gridspec.GridSpec(4, 3,
+gs = gridspec.GridSpec(3, 3,
                        width_ratios=[1, 1,1],
-                       height_ratios=[1, 0.7,1,3],
+                       height_ratios=[1.5,1,3],
                        wspace=0.1,
                        hspace=0.2
                        )
 ax1 = plt.subplot(gs[0,:])
-ax2 = plt.subplot(gs[1,:])
-ax3 = plt.subplot(gs[2,:])
-ax4 = plt.subplot(gs[3,0])
-ax5 = plt.subplot(gs[3,1])
-ax6 = plt.subplot(gs[3,2])
+ax3 = plt.subplot(gs[1,:])
+ax4 = plt.subplot(gs[2,0])
+ax5 = plt.subplot(gs[2,1])
+ax6 = plt.subplot(gs[2,2])
 
 
 ax1.plot(image_ana.roi_array, linewidth=2)
@@ -182,16 +185,17 @@ if image_ana.stop_number != 0:
     ax1.axvspan(image_ana.stop_number,len(image_ana.data1) , facecolor='black', alpha=0.5)
 
 ax1.axhline(y=image_ana.upper_threshold, linewidth=1, color = 'green')
-ax1.axhline(y=image_ana.lower_threshold, linewidth=1, color = 'green')        
+ax1.axhline(y=image_ana.lower_threshold, linewidth=1, color = 'green') 
+ax1.axvline(x =background_file_number, linewidth=1, color = 'green')        
+data5_ax1 = ax1.twinx()
+data5_ax1.plot(image_ana.data5 , linewidth=0.8, color ="r", label="B field")
+data6_ax1 = ax1.twinx()
+data6_ax1.plot(image_ana.data6 , linewidth=0.8, color ="g", label="Y target temp")
 
-
-ax1.tick_params(labelbottom="off")
-
-ax2.plot(image_ana.data3 , linewidth=0.8, color ="r")
 data1_ax3 = ax3.twinx()
 data1_ax3.plot(image_ana.data1, linewidth=0.8, color ="c", label="I2")
 data2_ax3 = ax3.twinx()
-data2_ax3.plot(image_ana.data2 , linewidth=0.8, color ="b", label="scan signal")
+data2_ax3.plot(image_ana.data2 , linewidth=0.4, color ="b", label="scan signal")
 data3_ax3 = ax3.twinx()
 data3_ax3.plot(image_ana.data3 , linewidth=0.8, color ="g", label="resonator")
 data7_ax3 = ax3.twinx()
@@ -206,13 +210,8 @@ data3_ax3.legend(loc=3)
 data7_ax3.legend(loc=4)
 data8_ax3.legend(loc=5)
 
-
-ax2.plot(image_ana.data5 , linewidth=0.8, color ="r", label="B field")
-data6_ax2 = ax2.twinx()
-data6_ax2.plot(image_ana.data6 , linewidth=0.8, color ="g", label="Y target temp")
-
-ax2.legend(loc=0)
-data6_ax2.legend(loc=1)
+data5_ax1.legend(loc=0)
+data6_ax1.legend(loc=1)
 
 upper_img_fig = ax4.imshow(image_ana.upper_img, vmin = vmin0, vmax = vmax0,cmap=cm.jet, interpolation='bicubic')
 lower_img_fig = ax5.imshow(image_ana.lower_img, vmin = vmin0, vmax = vmax0,cmap=cm.jet, interpolation='bicubic')
